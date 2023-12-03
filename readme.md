913dc3dd2282299b0319fade1ab4c420ed849856
mytin-design <denmytin@gmail.com>
2023-12-03 16:58:58 +0300

    buildingprojects

---
+++ readme.md
@@ -0,0 +1,43 @@
+# Notes
+# Objective 1
+## Js
+saveComment function
+1. get the input element for new notes;
+2. Store the value of the input to a variable;
+3. Create a paragraph element - to host the new comment value
+4. Let the paragraph's value be the value from the input;
+5. Get the container that hosts all the notes;
+6. Append the paragraph element
+
+
+# Objective 2
+### random Id and classes
+
+To create paragraphs/comments with unique ids or classes
+
+# On hover, the the note should provide an option to delete, edit, etc
+
+# Option 2 - Or onclick, the note should provide the edit actions banner? - implemented
+
+- Option 2 Objective complete;
+
+Bug del001 arising;
+
+When we add another note, the deleted note disappears;
+
+How do we make a note in the deleted section stay there forever
+
+//okay whats happenning ---
+-every value passed through the input is stored in the variable pcom1
+
+Therefore, how can we put every value in a new variable;
+
+
+//Suggested strategy
+list the values in an array;
+use array.pop(), etc functions to add or delete items from array
+
+
+
+
+
---
+++ script.js
@@ -0,0 +1,70 @@
+
+//save comment by adding it to the my notes section
+
+//get note from input
+    let newcom = document.getElementById("noteId");
+
+    //create a paragraph to store the new notes
+    let pcom1 = document.createElement("p");
+
+    //get container hosting notes- notes will be appended here
+    let innotes = document.querySelector(".innernotes");
+
+    let commentsList = [];
+let saveComment = () => {
+    //get value
+
+
+    let newcomm = newcom.value;
+
+    pcom1.setAttribute("class", "editacts");
+    pcom1.innerHTML = newcomm;
+    
+    //get mynotes box
+
+    innotes.appendChild(pcom1);
+
+    //onclick, the note should display the edit banner
+    pcom1.setAttribute("onclick", "showBanner()");
+
+}
+
+
+//get container with action btns
+    let accont = document.querySelector(".editactions");
+
+
+let showBanner = () => {
+    
+    accont.classList.remove("hideacts"); //remove the class - because it hosts the display:none declaration
+}
+
+//when actions appear;
+//close button should remove the note from the main section and add it in the deleted section
+
+let dlist = document.querySelector(".deletedbox");
+let deleteThis = () => {
+    let deleted = pcom1;
+    innotes.removeChild(deleted);
+
+    //add to delete list
+    dlist.appendChild(deleted);
+    accont.classList.add("hideacts");
+    deleted.removeAttribute("onclick", "showBanner()");
+    noted.removeAttribute("contenteditable", "true");
+
+}
+
+//edit note
+let noted = pcom1;
+
+let editNote = () => {
+    let noted = pcom1;
+    noted.setAttribute("contenteditable", "true");
+}
+
+
+
+
+
+
